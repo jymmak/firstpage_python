475f80760a24040c27b236b02dbfb6bece9004c8
Jymma <jkmogollon.cp@gmail.com>
2019-09-12 15:56:43 -0300

    add structure my first page

---
+++ index.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Home page'
+
+@app.route('/about')
+def about():
+    return 'About page'
+    
+if __name__ == '__main__':
+    app.run()
+
+
+
